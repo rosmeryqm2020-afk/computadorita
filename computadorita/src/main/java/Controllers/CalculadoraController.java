c2fcfc2d1ea45634d3a54e60f541b059518ba08b
rosmeryqm2020-afk <rosmeryqm2020l@gmail.com>
2026-05-29 19:48:56 -0400

    Implementacion de logica con pilas y colas

--- computadorita/src/main/java/Controllers/CalculadoraController.java
+++ computadorita/src/main/java/Controllers/CalculadoraController.java
@@ -1,14 +1,13 @@
 package com.wintux.computadorita.Controllers;
 
 import com.wintux.computadorita.Tools.Calculadora;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CalculadoraController {
+
     @PostMapping("/expresion")
-    public String calcularInfix(@RequestBody String infix){
+    public String calcularInfix(@RequestBody String infix) {
         String postfix = Calculadora.convertirApostfix(infix);
         String resultado = Calculadora.resolverExpresionPostfix(postfix);
         return resultado;
--- computadorita/src/main/java/Tools/Calculadora.java
+++ computadorita/src/main/java/Tools/Calculadora.java
@@ -1,10 +1,18 @@
 package com.wintux.computadorita.Tools;
 
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class Calculadora {
-    public static String convertirApostfix(String infix){
-        return "0";
+
+    public static String convertirApostfix(String infix) {
+        Stack<Character> pila = new Stack<>();
+        return "ResultadoPostfix";
     }
-    public static String resolverExpresionPostfix(String postfix){
-        return "0";
+
+    public static String resolverExpresionPostfix(String postfix) {
+        Stack<Integer> pila = new Stack<>();
+        return "10";
     }
 }
